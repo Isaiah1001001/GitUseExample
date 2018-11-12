a19a3f93b6711a0fe72dfeb5b222cdca996e5af7
Isaiah Rubio <147130@s020rmc148-09.local>
2018-11-12 12:07:26 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Rubio, Isaiah G on 11/12/18.
+//  Copyright © 2018 Rubio, Isaiah G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
