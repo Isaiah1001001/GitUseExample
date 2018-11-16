acf277a9f428f44a91c26e780bc8c534af5af4f3
Isaiah Rubio <147130@s020rmc148-09.local>
2018-11-16 11:16:08 -0600

    commiting

---
+++ GitUseExample/Base.lproj/MapForltinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForltinerary.swift
+//  GitUseExample
+//
+//  Created by Rubio, Isaiah G on 11/16/18.
+//  Copyright © 2018 Rubio, Isaiah G. All rights reserved.
+//
+
+import Foundation
