0ad439e1698524bd6d9f6ce4c8d98ec9d100ba14
Isaiah Rubio <147130@s020rmc148-09.local>
2018-11-16 11:31:13 -0600

    try

--- GitUseExample/AppDelegate.swift
+++ GitUseExample/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("Application did finish launching")
-        return true
+        return true 
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
